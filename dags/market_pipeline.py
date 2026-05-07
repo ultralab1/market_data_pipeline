136ea41a47567f022dfb0c05528e0e239c91eabf
ultralab1 <ra.Ouryuken@gmail.com>
2026-05-07 04:17:06 -0400

    Fixed up syntax

--- dags/market_pipeline.py
+++ dags/market_pipeline.py
@@ -4,25 +4,26 @@ from airflow.sdk import dag, task
 from ingestion.scripts.load_raw import load_crypto, load_stocks
 from datetime import datetime, timedelta
 
-@dag(start_date=datetime(2026, 5, 8), catchup=False):
+@dag(schedule='*/15 * * * *',start_date=datetime(2026, 5, 8), catchup=False)
 def market_pipeline():
     @task
-    def load_data():
+    def stocks():
         tickers = ['AAPL','AMD','MSFT']
         load_stocks(tickers=tickers,
                     period='1m',
                     interval='1m')
-        load_crypto()
     @task
-    def run_dbt():
-        try:
-            run_command = BashOperator(task_id='dbt_run',
-                                        bash_command='dbt run --project-dir dbt/market_data')
-            test_command = BashOperator(task_id='dbt_test',
-                                        bash_command='dbt test --project-dir dbt/market_data')
-        except airflow.sdk.exceptions.AirflowSkipException as e:
-            print('Command failed, skipping')
-            print(f'{e}')
-        except airflow.sdk.exceptions.AirflowException as e:
-            print('Command failed')
-            print(f'{e}')
+    def crypto():
+        load_crypto()
+
+    run_command = BashOperator(task_id='dbt_run',
+                                bash_command='dbt run --project-dir dbt/market_data',
+                               retries=3)
+    test_command = BashOperator(task_id='dbt_test',
+                                bash_command='dbt test --project-dir dbt/market_data',
+                                retries=3)
+    stocks_task = stocks()
+    crypto_task = crypto()
+    [stocks_task, crypto_task] >> run_command >> test_command
+
+market_pipeline()
